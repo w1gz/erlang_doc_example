5b46f73e78ad451091ec500cf9acb710769fef8f
w1gz <w1gz@users.noreply.github.com>
2016-04-19 11:31:12 +0200

    Initial commit

---
+++ c-port/port.c
@@ -0,0 +1,24 @@
+/* port.c */
+/* #include "complex.c" */
+/* #include "erl_comm.c" */
+
+typedef unsigned char byte;
+
+int main() {
+    int fn, arg, res;
+    byte buf[100];
+
+    while (read_cmd(buf) > 0) {
+        fn = buf[0];
+        arg = buf[1];
+
+        if (fn == 1) {
+            res = foo(arg);
+        } else if (fn == 2) {
+            res = bar(arg);
+        }
+
+        buf[0] = res;
+        write_cmd(buf, 1);
+    }
+}
